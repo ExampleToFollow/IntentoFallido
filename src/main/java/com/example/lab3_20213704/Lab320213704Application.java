2fc761f21d19b4ff9f6ab02f9c75507e798c985b
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-18 20:43:49 -0500

    FIRST COMMIT

---
+++ src/main/java/com/example/lab3_20213704/Lab320213704Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20213704;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320213704Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320213704Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20213704/Lab320213704ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20213704;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320213704ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
